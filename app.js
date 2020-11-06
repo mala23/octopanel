cf84d366bc194c927f97bb9c5963c097020e986b
mala23 <mail@mala23.me>
2020-11-06 13:36:32 +0100

    stale state

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/blo*', (req, res) => {
+  res.render('index')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
