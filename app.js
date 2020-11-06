1c490e125e9222f080ba243a99ea769fc45ed61d
mala23 <mail@mala23.me>
2020-11-06 13:22:09 +0100

    create blowers

--- app.js
+++ app.js
@@ -2,11 +2,76 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var mqtt = require('mqtt');
+
+var client = mqtt.connect('mqtt://3d8f57f8:67f1ba13358e9b30@broker.shiftr.io', {
+  clientId: 'octopanel'
+});
+
+client.on('connect', function(){
+  console.log('client has connected!');
+
+  client.subscribe('/octoblo');
+
+});
+
+client.on('message', function(topic, message) {
+  console.log('new message:', topic, message.toString());
+});
+
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/blo1', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo1')
+  console.log('/octoblo/blowers', 'blo1')
+})
+
+app.get('/blo2', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo2')
+  console.log('/octoblo/blowers', 'blo2')
+})
+
+app.get('/blo3', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo3')
+  console.log('/octoblo/blowers', 'blo3')
+})
+
+app.get('/blo4', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo4')
+  console.log('/octoblo/blowers', 'blo4')
+})
+
+app.get('/blo5', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo5')
+  console.log('/octoblo/blowers', 'blo5')
+})
+
+app.get('/blo6', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo6')
+  console.log('/octoblo/blowers', 'blo6')
+})
+
+app.get('/blo7', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo7')
+  console.log('/octoblo/blowers', 'blo7')
+})
+
+app.get('/blo8', (req, res) => {
+  res.render('index')
+  client.publish('/octoblo/blowers', 'blo8')
+  console.log('/octoblo/blowers', 'blo8')
 })
 
 app.listen(port, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,105 @@
 console.log('Client-side code running');
 
+
+
 const blo1 = document.getElementById('blo1');
 blo1.addEventListener('click', function(e) {
+
   document.getElementById("blo1").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo1','active');
+  } else {
+    alert('local storage not supported');
+  }
 });
 
-document.getElementById('myButton').onclick = function() {
+const blo2 = document.getElementById('blo2');
+blo2.addEventListener('click', function(e) {
+
+  document.getElementById("blo2").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo2','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo3 = document.getElementById('blo3');
+blo3.addEventListener('click', function(e) {
+
+  document.getElementById("blo3").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo3','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo4 = document.getElementById('blo4');
+blo4.addEventListener('click', function(e) {
+
+  document.getElementById("blo4").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo4','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo5 = document.getElementById('blo4');
+blo5.addEventListener('click', function(e) {
+
+  document.getElementById("blo5").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo5','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo6 = document.getElementById('blo4');
+blo6.addEventListener('click', function(e) {
+
+  document.getElementById("blo6").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo6','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo7 = document.getElementById('blo4');
+blo7.addEventListener('click', function(e) {
+
+  document.getElementById("blo7").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo7','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+const blo8 = document.getElementById('blo4');
+blo8.addEventListener('click', function(e) {
+
+  document.getElementById("blo8").className = "blower__active"
+
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem('blo8','active');
+  } else {
+    alert('local storage not supported');
+  }
+});
+
+/*
+document.getElementById('blo1').onclick = function() {
 
     var className = ' ' + myButton.className + ' ';
 
@@ -15,3 +109,4 @@ document.getElementById('myButton').onclick = function() {
         this.className += ' active';
     }              
 }
+*/
